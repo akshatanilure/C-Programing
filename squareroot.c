9054b12fb75a35c603e25d3ae8210f41fb38377e
akshatanilure <akshatanilure2211@gmail.com>
2024-08-23 12:18:49 +0530

    Create squareroot.c
    C Program to find the  square root of number 49

---
+++ squareroot.c
@@ -0,0 +1,10 @@
+#include<math.h>
+#include<stdio.h>
+int main()
+{
+ double number;
+ number = 49;
+ double squareroot = sqrt(number);
+ printf("the square root of %.21f = %.21f",number,squareroot);
+ return 0;
+}
