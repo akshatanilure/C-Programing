e828730c613018f61e9728837f2d2c35eac4e237
akshatanilure <akshatanilure2211@gmail.com>
2024-08-23 17:18:21 +0530

    C program using function
    This program is about my introduction using function

---
+++ funcintro.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+ void introduction()
+{
+   printf("hello");
+   printf("my name is akshata nilure");
+   printf("I study engineering CS branch in SDM college Dharwad");
+}
+
+int main()
+{
+  introduction();
+  return 0;
+}
+
+ 
