a1b4171fa7bb97b70b884b6b34e8bf39376d2687
akshatanilure <akshatanilure2211@gmail.com>
2024-08-27 18:32:52 +0530

    C program to find the size of different pointer type
    C program to find the size of different pointer type

---
+++ SizeofDiffType.c
@@ -0,0 +1,37 @@
+// dummy structure
+struct str {
+};
+
+#include<stdio.h>
+struct str{
+};
+void func(int a, int b){
+};
+
+int main()
+{
+  
+    int a = 10;
+    char c = 'G';
+    struct str x;
+    
+    int* ptr_int = &a;
+    char* ptr_char = &c;
+    struct str* ptr_str = &x;
+    void (*ptr_func)(int, int) = &func;
+    void* ptr_vn = NULL;
+
+   
+    printf("Size of Integer Pointer  \t:\t%d bytes\n",
+           sizeof(ptr_int));
+    printf("Size of Character Pointer\t:\t%d bytes\n",
+           sizeof(ptr_char));
+    printf("Size of Structure Pointer\t:\t%d bytes\n",
+           sizeof(ptr_str));
+    printf("Size of Function Pointer\t:\t%d bytes\n",
+           sizeof(ptr_func));
+    printf("Size of NULL Void Pointer\t:\t%d bytes",
+           sizeof(ptr_vn));
+
+    return 0;
+}
